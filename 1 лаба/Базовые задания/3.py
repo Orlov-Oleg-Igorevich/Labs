a5fe37787e56e5afd6556194c472a39abcfd5933
Orlov Oleg Igorevich <145780861+Orlov-Oleg-Igorevich@users.noreply.github.com>
2024-05-12 16:03:22 +0300

    Update 3.py

--- 1 лаба/Базовые задания/3.py
+++ 1 лаба/Базовые задания/3.py
@@ -1,9 +1,11 @@
-def arv(num: list):
-    return sum(num)/len(num)
-list_ = [1, 2, 3, 4, 5]
-print("Среднее значение этих 5 чисел равно %.5f" % (arv(list_)))
+sum_ = 1+2+3+4+5
+count = 5
+arv = sum_/count
+print("Среднее значение этих 5 чисел равно %.5f" % (arv))
 x = float(input("Добавьте ещё число: "))
 while x:
-    list_.append(x)
-    print("Среднее значение этих %.i чисел равно %.5f" % (len(list_), arv(list_)))
+    sum_ += x
+    count += 1
+    arv = sum_/count
+    print("Среднее значение этих %.i чисел равно %.5f" % (count, arv))
     x = float(input())
